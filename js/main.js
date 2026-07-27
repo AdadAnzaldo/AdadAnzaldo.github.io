963051c48b25389448d844427deeefa6e0bbfab8
AdadAnzaldo <adad.anzaldo@gmail.com>
2026-07-27 14:52:40 -0600

    hamburger navbar
    hamburger navbar for phones

--- js/main.js
+++ js/main.js
@@ -168,3 +168,18 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navLinks.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links a").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navLinks.classList.remove("active");
+}));
